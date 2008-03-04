e279e7682306b4318e3c1fa97f4734cf14c7d66f
berrynagoya <berrynagoya@f740badb-1843-0410-85a5-65dffd621859>
2008-03-04 02:13:20 +0000

    Removed strings from XUL files to DTD files

--- trunk/SEMTools/semtools/content/toolbar-overlay.js
+++ trunk/SEMTools/semtools/content/toolbar-overlay.js
@@ -1,27 +1,3 @@
-var adhighlighter = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-    this.strings = document.getElementById("adhighlighter-strings");
-    document.getElementById("contentAreaContextMenu")
-            .addEventListener("popupshowing", function(e) { this.showContextMenu(e); }, false);
-  },
-
-  showContextMenu: function(event) {
-    // show or hide the menuitem based on what the context menu is on
-    // see http://kb.mozillazine.org/Adding_items_to_menus
-    document.getElementById("context-adhighlighter").hidden = gContextMenu.onImage;
-  },
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
-  },
-
-};
-window.addEventListener("load", function(e) { adhighlighter.onLoad(e);} , false);
-
 
 //Set the two images for on and off for the adcounter
 var adCounterOnImage = "chrome://adhighlighter/content/adcounter_status_on.gif";
